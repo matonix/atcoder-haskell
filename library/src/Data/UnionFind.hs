165064fe294e90a351716313e24c1815e72dc597
matonix <automatonix@gmail.com>
2019-03-08 23:55:38 +0900

    add unionfind with array

---
+++ library/src/Data/UnionFind.hs
@@ -0,0 +1,53 @@
+module Data.UnionFind where
+
+import Data.Array.IO
+
+data UnionFind = UnionFind
+  { parent :: IOUArray Int Int
+  , size :: IOUArray Int Int
+  }
+
+initUf :: Int -> IO UnionFind
+initUf n = UnionFind
+  <$> newListArray (1, n) [1..n]
+  <*> newArray (1, n) 1
+
+rootUf :: UnionFind -> Int -> IO (UnionFind, Int)
+rootUf uf x = do
+  px <- readArray (parent uf) x
+  if px == x 
+  then return (uf, x)
+  else do
+    (UnionFind parent' size', rx) <- rootUf uf px
+    writeArray parent' x rx
+    return (UnionFind parent' size', rx)
+
+sameUf :: UnionFind -> Int -> Int -> IO (UnionFind, Bool)
+sameUf uf x y = do
+  (uf', rx) <- rootUf uf x
+  (uf'', ry) <- rootUf uf' y
+  return (uf'', rx == ry)
+
+uniteUf :: UnionFind -> Int -> Int -> IO UnionFind
+uniteUf uf x y = do
+  (uf', x') <- rootUf uf x
+  (uf''@(UnionFind parent' size'), y') <- rootUf uf' y
+  if x' == y'
+  then return uf''
+  else do
+    sizeX <- readArray size' x'
+    sizeY <- readArray size' y'
+    if sizeX < sizeY
+    then do
+      writeArray parent' x' y'
+      writeArray size' y' (sizeX + sizeY)
+      return $ UnionFind parent' size'
+    else do
+      writeArray parent' y' x'
+      writeArray size' x' (sizeX + sizeY)
+      return $ UnionFind parent' size'
+    
+sizeUf :: UnionFind -> Int -> IO Int
+sizeUf uf x = do
+  (UnionFind _ size', x') <- rootUf uf x
+  readArray size' x'
---
+++ library/test/Data/UnionFindSpec.hs
@@ -0,0 +1,29 @@
+module Data.UnionFindSpec where
+
+import Data.Array.IO
+import Data.UnionFind
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "initUf" $ do
+    it "initial parent of 3rd item should be 3" $ do
+      pick3rd `shouldReturn` 3
+  describe "uniteUf" $ do
+    it "size of united 3 item should be 3" $ do
+      unite3 `shouldReturn` 3
+
+pick3rd :: IO Int
+pick3rd = do
+  uf <- initUf 3
+  readArray (parent uf) 3
+
+unite3 :: IO Int
+unite3 = do
+  uf <- initUf 3
+  uf' <- uniteUf uf 1 2
+  uf'' <- uniteUf uf' 1 3
+  sizeUf uf'' 3
